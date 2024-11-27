73f21f668f862a91bbbc9fcd15285e94b09f579f
Pablo Ordóñez <pablosebas61@gmail.com>
2024-11-27 11:30:01 -0500

    Cambios calificación usando if y switch

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -31,14 +31,16 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (s) (si) "
+                    + "(yes) (y) si desea salir del ciclo ");
             salida = entrada.nextLine();
-
+            if(salida.equals("s")||salida.equals("si")||salida.equals("yes")||
+                    salida.equals("y")){
+                bandera = false;
+            }
             
-            
+        } while (bandera==true); // (bandera==true)
 
-        } while (bandera); // (bandera==true)
-
-        System.out.printf("Listado de Notas\n%s\n", cadenaFinal);
+        System.out.printf("Listado de Notas:\n%s\n", cadenaFinal);
     }
 }
