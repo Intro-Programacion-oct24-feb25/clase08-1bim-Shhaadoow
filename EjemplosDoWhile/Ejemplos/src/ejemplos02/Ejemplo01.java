cfea8d3ca3c2500a2a743c24d3b7a624c63c160c
Pablo Ordóñez <pablosebas61@gmail.com>
2024-11-27 11:13:28 -0500

    Cambios peques

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
@@ -21,14 +21,16 @@ package ejemplos02;
 public class Ejemplo01 {
 
     public static void main(String[] args) {
-        int contador = 1;
+        int contador = 0;
         int suma;
         suma = 0;
         do {
+            System.out.printf("%d\n", contador);
             suma = suma + contador;
             contador = contador + 1;
+            
         } while (contador <= 10); 
-        System.out.printf("%d\n", suma);
+        System.out.printf("\n%d\n", suma);
     }
 
 }
